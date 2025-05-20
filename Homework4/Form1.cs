ecc2bf346b311eaf686c827c280d89d37ccab2ac
Sadra <sadrajabbarisani@gmail.com>
2025-05-20 15:05:53 +0330

    Homework 4 done!

--- Homework4/Form1.cs
+++ Homework4/Form1.cs
@@ -12,9 +12,76 @@ namespace Homework4
 {
     public partial class Form1 : Form
     {
+        Ratio A, B, result;
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio();
+            A.A = int.Parse(AATB.Text);
+            A.B = int.Parse(ABTB.Text);
+
+            B = new Ratio();
+            B.A = int.Parse(BATB.Text);
+            B.B = int.Parse(BBTB.Text);
+
+            result = A.add(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio();
+            A.A = int.Parse(AATB.Text);
+            A.B = int.Parse(ABTB.Text);
+
+            B = new Ratio();
+            B.A = int.Parse(BATB.Text);
+            B.B = int.Parse(BBTB.Text);
+
+            Ratio helpRatio = new Ratio();
+            helpRatio.A = -1;
+            helpRatio.B = 1;
+
+            B = B.Mul(helpRatio);
+
+            result = A.add(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void mulBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio();
+            A.A = int.Parse(AATB.Text);
+            A.B = int.Parse(ABTB.Text);
+
+            B = new Ratio();
+            B.A = int.Parse(BATB.Text);
+            B.B = int.Parse(BBTB.Text);
+
+            result = A.Mul(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void dividBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Homework4/Ratio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    internal class Ratio
+    {
+        public int A;
+        public int B;
+
+        public Ratio add(Ratio r)
+        {
+            Ratio result = new Ratio();
+
+            result.B = this.B * r.B;
+            result.A = this.A * r.B + r.A * this.B;
+
+            return result;
+        }
+
+        public Ratio add(int a)
+        {
+            Ratio result = new Ratio();
+
+            result.B = this.B;
+            result.A = this.A + a * this.B;
+            
+            return result;
+        }
+
+        public Ratio Mul(Ratio r)
+        {
+            Ratio result = new Ratio();
+            result.B = this.B * r.B;
+            result.A = this.A * r.A;
+            return result;
+        }
+
+        public Ratio Mul(int a)
+        {
+            Ratio result = new Ratio();
+            result.B = this.B;
+            result.A = a * this.A;
+            return result;
+        }
+
+    }
+}
