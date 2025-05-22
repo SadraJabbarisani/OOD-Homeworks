b4f354e6f5cd0eb9300a58bac5147732fe7c74a0
Sadra <sadrajabbarisani@gmail.com>
2025-05-22 07:24:24 +0330

    Homework6 added: it is Homework4 with constructors

---
+++ Homework6/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Homework6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AATB = new System.Windows.Forms.TextBox();
+            this.ABTB = new System.Windows.Forms.TextBox();
+            this.BATB = new System.Windows.Forms.TextBox();
+            this.BBTB = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultTextA = new System.Windows.Forms.Label();
+            this.ResultTextB = new System.Windows.Forms.Label();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.mulBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AATB
+            // 
+            this.AATB.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AATB.Location = new System.Drawing.Point(70, 110);
+            this.AATB.Name = "AATB";
+            this.AATB.Size = new System.Drawing.Size(100, 80);
+            this.AATB.TabIndex = 0;
+            this.AATB.Text = "0";
+            this.AATB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ABTB
+            // 
+            this.ABTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ABTB.Location = new System.Drawing.Point(70, 214);
+            this.ABTB.Name = "ABTB";
+            this.ABTB.Size = new System.Drawing.Size(100, 80);
+            this.ABTB.TabIndex = 1;
+            this.ABTB.Text = "1";
+            this.ABTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BATB
+            // 
+            this.BATB.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BATB.Location = new System.Drawing.Point(335, 110);
+            this.BATB.Name = "BATB";
+            this.BATB.Size = new System.Drawing.Size(100, 80);
+            this.BATB.TabIndex = 2;
+            this.BATB.Text = "0";
+            this.BATB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BBTB
+            // 
+            this.BBTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BBTB.Location = new System.Drawing.Point(335, 214);
+            this.BBTB.Name = "BBTB";
+            this.BBTB.Size = new System.Drawing.Size(100, 80);
+            this.BBTB.TabIndex = 3;
+            this.BBTB.Text = "1";
+            this.BBTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(23, 196);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 11);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(283, 196);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 11);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(588, 196);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 11);
+            this.panel3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(509, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 73);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "=";
+            // 
+            // ResultTextA
+            // 
+            this.ResultTextA.AutoSize = true;
+            this.ResultTextA.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultTextA.ForeColor = System.Drawing.Color.White;
+            this.ResultTextA.Location = new System.Drawing.Point(649, 108);
+            this.ResultTextA.Name = "ResultTextA";
+            this.ResultTextA.Size = new System.Drawing.Size(68, 73);
+            this.ResultTextA.TabIndex = 8;
+            this.ResultTextA.Text = "0";
+            // 
+            // ResultTextB
+            // 
+            this.ResultTextB.AutoSize = true;
+            this.ResultTextB.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultTextB.ForeColor = System.Drawing.Color.White;
+            this.ResultTextB.Location = new System.Drawing.Point(649, 221);
+            this.ResultTextB.Name = "ResultTextB";
+            this.ResultTextB.Size = new System.Drawing.Size(68, 73);
+            this.ResultTextB.TabIndex = 9;
+            this.ResultTextB.Text = "1";
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusBtn.Location = new System.Drawing.Point(236, 179);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(43, 44);
+            this.minusBtn.TabIndex = 10;
+            this.minusBtn.Text = "-";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusBtn.Location = new System.Drawing.Point(236, 120);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(43, 44);
+            this.plusBtn.TabIndex = 11;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // mulBtn
+            // 
+            this.mulBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mulBtn.Location = new System.Drawing.Point(236, 232);
+            this.mulBtn.Name = "mulBtn";
+            this.mulBtn.Size = new System.Drawing.Size(43, 44);
+            this.mulBtn.TabIndex = 12;
+            this.mulBtn.Text = "x";
+            this.mulBtn.UseVisualStyleBackColor = true;
+            this.mulBtn.Click += new System.EventHandler(this.mulBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mulBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.minusBtn);
+            this.Controls.Add(this.ResultTextB);
+            this.Controls.Add(this.ResultTextA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BBTB);
+            this.Controls.Add(this.BATB);
+            this.Controls.Add(this.ABTB);
+            this.Controls.Add(this.AATB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AATB;
+        private System.Windows.Forms.TextBox ABTB;
+        private System.Windows.Forms.TextBox BATB;
+        private System.Windows.Forms.TextBox BBTB;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ResultTextA;
+        private System.Windows.Forms.Label ResultTextB;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button mulBtn;
+    }
+}
+
---
+++ Homework6/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework6
+{
+    public partial class Form1 : Form
+    {
+        Ratio A, B, result;
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio(int.Parse(AATB.Text), int.Parse(ABTB.Text));
+            B = new Ratio(int.Parse(BATB.Text), int.Parse(BBTB.Text));
+
+            result = A.add(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio(int.Parse(AATB.Text), int.Parse(ABTB.Text));
+            B = new Ratio(int.Parse(BATB.Text), int.Parse(BBTB.Text));
+
+            Ratio helpRatio = new Ratio(-1, 1);
+
+            B = B.Mul(helpRatio);
+
+            result = A.add(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void mulBtn_Click(object sender, EventArgs e)
+        {
+            A = new Ratio(int.Parse(AATB.Text), int.Parse(ABTB.Text));
+            B = new Ratio(int.Parse(BATB.Text), int.Parse(BBTB.Text));
+
+            result = A.Mul(B);
+
+            ResultTextA.Text = result.A.ToString();
+            ResultTextB.Text = result.B.ToString();
+        }
+
+        private void dividBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
